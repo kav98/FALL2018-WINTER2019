e8ccfb4122835b9354a2457f11c416a6b2050cdc
kav98 <kavya.raman@uoit.net>
2018-11-19 07:46:59 -0500

    update progress

--- SOFE3200/assignment2/problem1.c
+++ SOFE3200/assignment2/problem1.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
-int main(){
+int main( int argc, char *argv[] ) {
+
+    int fd1, fd2;
+
+    fd1 = open(agrv[1],O_CREAT | O_RDONLY);
+    if(fd1==-1){ //handle error condition
+        printf("ERROR: error when opening file")
+    }
+
+    
 
 }
