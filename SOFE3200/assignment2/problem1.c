875bf43ff8c34b37fb7f66e03e5ba7d4c85e178e
kav98 <kavya.raman@uoit.net>
2018-11-19 22:25:32 -0500

    pushing problem 1

--- SOFE3200/assignment2/problem1.c
+++ SOFE3200/assignment2/problem1.c
@@ -1,35 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <syscall.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 
 int main( int argc, char *argv[] ) {
 
     int fd1, fd2;
+    char buf[1024];
+    long int cpfile;
+    int bytes 
 
-    fd1 = open(agrv[1],O_CREAT | O_RDONLY);
+    fd1 = open(argv[1], O_RDONLY)
     if(fd1==-1){ //handle error condition
-        printf("ERROR: error when opening source file")
+        perror("ERROR: error when opening source file");  
+        exit(1);
     }
 
-    void *buf = (*char) malloc(100);
-
-    int bytes = read(fd1,buf,100);
-
-    printf('bytes copied: %d, buffer: %s', bytes, buf);
-
-    close(fd1);
+    printf('%s/n,%d', buf, fd1);
+    
 
     fd2 = open(argv[2], O_CREAT | O_WRONLY);
     if(fd2==-1){ //handle error condition
-        printf("ERROR: error when opening target file")
-    }
-    int cpfile;
-    while(bytes=read(fd1,buf,100)>0){
-        cpfile=write(fd2,buf,100);
-    }
-    if(cpfile==-1){ //handle error condition
-        printf("ERROR: error when writing to target file")
+        perror("ERROR: error when opening target file");
+        exit(1);
     }
 
+    
+    while((bytes=read(fd1,buf,1024))>0){
+        cpfile=write(fd2,buf,1024);
+    }
+    if(cpfile==-1){ //handle error condition
+        printf("ERROR: error when writing to target file");
+    }
+    close(fd1);
     close(fd2);
 
 
